35aa81ba3a112c970aa29f8cdd879892b22182e2
Nazarii Lysak <nazarii.lysak@ua.nestle.com>
2024-12-22 20:24:46 +0200

    add registrations for user / lserl list

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { MarketsModule } from './markets/markets.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MarketsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    MarketsModule, 
+    AuthModule
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [AppController],
 })
 export class AppModule {}
